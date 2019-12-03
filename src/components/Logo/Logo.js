61be6f843d7dd1a2d877afc9d94f2bf6db4eb2f9
Harry Street <streeter11@hotmail.com>
2019-12-03 17:51:26 +0000

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+ 
+
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+       <div className="Tilt-inner"> <span role="img">👽</span> </div>
+      </Tilt>
+    </div>
+  );
+}
+
+export default Logo;
