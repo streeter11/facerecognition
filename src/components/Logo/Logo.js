2af173051fcf19ed3d07602071b2a8fcb308122b
Harry Street <streeter11@hotmail.com>
2019-12-04 19:08:49 +0000

    update package json

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import Tilt from 'react-tilt';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+      <Tilt className="Tilt br3 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
        <div className="Tilt-inner"> <span role="img">👽</span> </div>
       </Tilt>
     </div>
